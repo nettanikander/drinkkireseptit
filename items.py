ba932d994a0f768a16f6039aa5ec78f542a57ca4
nettanikander <netta.nik11@gmail.com>
2025-09-18 19:46:21 +0300

    Show item list/info

--- items.py
+++ items.py
@@ -4,3 +4,17 @@ def add_item(title, ingredients, recipe, user_id):
     sql = """INSERT INTO items (title, ingredients, recipe, user_id)
             VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, ingredients, recipe, user_id])
+
+def get_items():
+    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT items.title,
+                    items.ingredients,
+                    items.recipe,
+                    users.username
+            FROM items, users
+            WHERE items.user_id = users.id AND
+                  items.id = ?"""
+    return db.query(sql, [item_id])[0]
