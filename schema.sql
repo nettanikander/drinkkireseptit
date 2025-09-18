765e257e18c76cf8a340ed34d8787a87c6275e86
nettanikander <netta.nik11@gmail.com>
2025-09-18 17:44:48 +0300

    Add new item form

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    ingredients TEXT,
+    recipe TEXT,
+    user_id INTEGER REFERENCES users
+);
